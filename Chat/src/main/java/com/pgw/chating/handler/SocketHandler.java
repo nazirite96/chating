c8192154ee3b91bda491787ad6f4d3c0a1037b26
nazirite96 <wkek805@naver.com>
2021-07-27 17:49:24 +0900

    불필요한 주석 제거 및 중복 코드 삭제
    이전 코드를 삭제하고 그에 따른 주석도 삭제했습니다.

--- Chat/src/main/java/com/pgw/chating/handler/SocketHandler.java
+++ Chat/src/main/java/com/pgw/chating/handler/SocketHandler.java
@@ -19,26 +19,10 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class SocketHandler extends TextWebSocketHandler {
 	
-	//HashMap<String, WebSocketSession> sessionMap = new HashMap<>(); //웹소켓 세션을 담아둘 맵
+	
 	List<HashMap<String, Object>> rls = new ArrayList<>(); //웹소켓 세션을 담아둘 리스트 ---roomListSessions
 	
-//	@Override
-//	public void handleTextMessage(WebSocketSession session, TextMessage message) {
-//		//메시지 발송
-//		//handleTextMessage 메소드는 메시지를 수신하면 실행됩니다. 상속받은 TextWebSocketHandler는 handleTextMessage를 실행시키며
-//		//메시지 타입에따라 handleBinaryMessage또는 handleTextMessage가 실행됩니다
-//		String msg = message.getPayload();
-//		JSONObject obj = jsonToObjectParser(msg);
-//		for(String key : sessionMap.keySet()) {
-//			WebSocketSession wss = sessionMap.get(key);
-//			try {
-//				wss.sendMessage(new TextMessage(obj.toJSONString()));
-//			}catch(Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		
-//	}
+
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) {
 		//메시지 발송
@@ -74,17 +58,7 @@ public class SocketHandler extends TextWebSocketHandler {
 		}
 	}
 	
-//	@Override
-//	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-//		//소켓 연결
-//		//afterConnectionEstablished 메소드는 웹소켓 연결이 되면 동작합니다.
-//		super.afterConnectionEstablished(session);
-//		sessionMap.put(session.getId(), session);
-//		JSONObject obj = new JSONObject();
-//		obj.put("type", "getId");
-//		obj.put("sessionId", session.getId());
-//		session.sendMessage(new TextMessage(obj.toJSONString()));
-//	}
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
@@ -123,24 +97,7 @@ public class SocketHandler extends TextWebSocketHandler {
 		session.sendMessage(new TextMessage(obj.toJSONString()));
 	}
 	
-//	@Override
-//	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-//		//소켓 종료
-//		//acafterConnectionClosed 메소드는 웹소켓이 종료되면 동작합니다.
-//		sessionMap.remove(session.getId());
-//		super.afterConnectionClosed(session, status);
-//	}
-//	
-//	private static JSONObject jsonToObjectParser(String jsonStr) {
-//		JSONParser parser = new JSONParser();
-//		JSONObject obj = null;
-//		try {
-//			obj = (JSONObject) parser.parse(jsonStr);
-//		} catch (ParseException e) {
-//			e.printStackTrace();
-//		}
-//		return obj;
-//	}
+
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		//소켓 종료
